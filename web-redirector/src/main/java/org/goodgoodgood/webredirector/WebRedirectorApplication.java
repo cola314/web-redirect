a18513181492e2fb147b1d4cd6327d89a3d3f03f
cola314 <ubj0314@naver.com>
2022-09-07 22:12:14 +0900

    feat: initialize project with spring initializr

---
+++ web-redirector/src/main/java/org/goodgoodgood/webredirector/WebRedirectorApplication.java
@@ -0,0 +1,13 @@
+package org.goodgoodgood.webredirector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRedirectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRedirectorApplication.class, args);
+	}
+
+}
---
+++ web-redirector/src/test/java/org/goodgoodgood/webredirector/WebRedirectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goodgoodgood.webredirector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRedirectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
